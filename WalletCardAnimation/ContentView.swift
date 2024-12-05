d310064660e5ca73062f21835f60cd3e4ded7337
Daniel Martinez <daniel@houlak.com>
2024-12-05 14:37:02 -0300

    chore: initial commit

---
+++ WalletCardAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletCardAnimation
+//
+//  Created by Daniel Martinez Condinanza on 5/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletCardAnimation/WalletCardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletCardAnimationApp.swift
+//  WalletCardAnimation
+//
+//  Created by Daniel Martinez Condinanza on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WalletCardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
